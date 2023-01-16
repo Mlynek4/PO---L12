a9d9a711a9b945dcfe79727b353bb17ce950e997
Mlynek4 <115485232+Mlynek4@users.noreply.github.com>
2023-01-16 18:20:24 +0100

    Create Main.java

---
+++ lesson11/Main.java
@@ -0,0 +1,8 @@
+
+public class Main
+{
+    public static void main(String[] args) {
+        String test = new String("Testing, TESTING");
+        System.out.println(new LowerCased(new Characters(test)).printedSeq());
+    }
+}
